13323fdb6ca5f02e973d01d723bb63c897a6b2bf
Konstantin <kostya13111997@rambler.ru>
2023-12-28 17:26:47 +0300

    New DZ

---
+++ Test_01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение M: ");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        PrintNaturalNumbers(m, n);
+
+        Console.ReadLine();
+    }
+
+    static void PrintNaturalNumbers(int m, int n)
+    {
+        if (m <= n)
+        {
+            Console.WriteLine(m);
+            PrintNaturalNumbers(m + 1, n);
+        }
+    }
+}
